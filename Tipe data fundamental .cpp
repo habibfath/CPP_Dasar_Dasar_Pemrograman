235873d16fab4ad639418c355cb962f416597bd1
Habib Nur Fathan <43870125+habibfath@users.noreply.github.com>
2019-08-11 04:30:16 +0700

    Add files via upload

---
+++ Tipe data fundamental .cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+using namespace std;
+
+
+
+int main()
+
+			
+{                  
+    int a = 1;
+    long b = 2;
+    short c = 3;
+	
+	cout << a << endl;
+	cout << sizeof(a) << " byte" << endl;
+	
+	cout << b << endl;
+	cout << sizeof(b) << " byte" << endl;	
+	
+	cout << c << endl;
+	cout << sizeof(c) << " byte" << endl;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+ 
+
+	return 0;
+} 
